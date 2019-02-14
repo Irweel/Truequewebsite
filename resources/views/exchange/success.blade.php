07a957151186ec86254ff0c9d907792da2d6cae1
Irweel <irweel@gmail.com>
2019-02-14 00:23:45 -0300

    fixes views

--- resources/views/exchange/success.blade.php
+++ resources/views/exchange/success.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.dev')
 @section('content')
- yay
-
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12 text-center">
+      <h1 class="mt-5">Enhorabuena {{ Auth::user()->name }}!</h1>
+      <p class="lead">Haz realizado una petición de intercambio, el usuario recibio una notificación con tu propuesta.</p>
+      <ul class="list-unstyled">
+        <li>
+          <a href="{{ url('/home') }}">Volver a la home</a>
+        </li>
+        <li>
+          <a href="{{ route('profile.user_profile', Auth::user()->id) }}">Mi Perfil</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
 @endsection
